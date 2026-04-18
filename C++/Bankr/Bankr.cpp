6064dd33445b2df9d6dc9db86dc42a163e3d15e5
yqi7somabf8 <gazeto729@gmail.com>
2026-04-18 09:28:40 +0200

    Testing a logger implementation. Also testing a massive code restructure. Also renamed the project to Bankr

--- C++/Bankr/Bankr.cpp
+++ C++/Bankr/Bankr.cpp
@@ -8,218 +8,22 @@
 #include <filesystem>
 #include <random>
 #include <cmath>
-
-class Account{
-private:
-    std::string name;
-    std::string password;
-    typedef struct{
-    int year; //dob = Date of Birth
-    int month;
-    int day;
-    } dob;
-    dob dateOfBirth;
-    int money = 0;
-    bool accountExists = false;
-    bool loggedIn = false;
-    bool debug = false;
-    double interest = 0;
-    int year = 67; //so unfunny tho
-
-public:
-    Account(std::string tempName) : name(tempName){}
-
-    Account(std::string name, std::string password, dob dateOfBirth, int initialMoney = 0) : name(name), password(password), dateOfBirth(dateOfBirth), accountExists(true), loggedIn(true), money(initialMoney){
-        this->generateInterestRate();
-        std::ifstream history("history.txt");
-        if(!history.is_open()){
-            this->debugOutput("Error creating history file!");
-        }
-        else {
-            this->debugOutput("History file found!");
-            history.close();
-        }
-        this->saveAccountToFile();
-    }
-
-    ~Account(){}                  
-
-    std::string getName(){return name;}
-    std::string getPassword(){return password;}
-    int getDobYear(){return dateOfBirth.year;}
-    int getDobMonth(){return dateOfBirth.month;}
-    int getDobDay(){return dateOfBirth.day;}
-    int getMoney(){return money;}
-    bool getAccountExists(){return accountExists;}
-    bool getLoggedIn(){return loggedIn;}
-    bool getDebug(){return debug;}
-    int getYear(){return year;}
-    double getInterest(){return interest;}
-    time_t getCurrentTime(){return std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());}
-    int getCurrentYear(){
-        std::time_t time = std::time(nullptr);
-        std::tm* tm = std::localtime(&time);
-        return tm->tm_year + 1900;
-    }
-    
-
-    void setName(std::string sName){name = sName;}
-    void setPassword(std::string sPassword){password = sPassword;}
-    void setDobYear(int sDobYear){dateOfBirth.year = sDobYear;}
-    void setDobMonth(int sDobMonth){dateOfBirth.month = sDobMonth;}
-    void setDobDay(int sDobDay){dateOfBirth.day = sDobDay;}
-    void setAccountExists(bool sAccountExists){accountExists = sAccountExists;}
-    void setLoggedIn(bool sLoggedIn){loggedIn = sLoggedIn;}
-    void setDebug(bool sDebug){debug = sDebug;}
-    void setYear(int sYear){year = sYear;}
-
-
-    void deposit(int amount){money += amount;}
-    void withdraw(int amount){money -= amount;}
-
-    void addHistory(std::string input){
-        std::ofstream history("history.txt", std::ios::app);
-        history << input << "\n";
-        history.close();
-    }
-
-    void deleteHistory(){
-        std::ofstream history("history.txt", std::ofstream::trunc);
-        history.close();
-    }
-
-    void debugOutput(std::string message){
-        if(this->debug){
-            std::cout << "[DEBUG] " << message << "\n";
-        }
-    }
-
-    void outputHistory(){
-        std::string output;
-        std::ifstream history("history.txt");
-        while(std::getline(history, output)){
-            std::cout << output << "\n";
-        }
-        history.close();
-    }
-
-    bool checkAccountFile(){
-        std::ifstream acc("Account.txt");
-        if(!acc.is_open() || std::filesystem::is_empty("Account.txt")){
-            this->debugOutput("Account file not found / no data!");
-            return false;
-        }
-        acc.close();
-        return true;
-    }
-
-    void saveAccountToFile(){
-        std::ofstream acc("Account.txt");
-        acc << "name=" << this->name << "\n" 
-            << "password=" << this->password << "\n" 
-            << "dob_day=" << this->dateOfBirth.day << "\n" 
-            << "dob_month=" << this->dateOfBirth.month << "\n" 
-            << "dob_year=" << this->dateOfBirth.year << "\n"
-            << "balance=" << this->money << "\n"
-            << "interest=" << this->interest << "\n";
-        acc.close();
-        this->debugOutput("Account data saved to file!");
-    }
-
-    void loadAccountFromFile(){
-        std::ifstream acc("Account.txt");
-        std::string data;
-        while(std::getline(acc, data)){
-            auto pos = data.find('=');
-            if(pos == std::string::npos) continue;
-
-            std::string key = data.substr(0, pos);
-            std::string value = data.substr(pos + 1);
-            if(key == "name") this->name = value;
-            else if(key == "password") this->password = value;
-            else if(key == "dob_day") this->dateOfBirth.day = std::stoi(value);
-            else if(key == "dob_month") this->dateOfBirth.month = std::stoi(value);
-            else if(key == "dob_year") this->dateOfBirth.year = std::stoi(value);
-            else if(key == "balance") this->money = std::stoi(value);
-            else if(key == "interest") this->interest = std::stod(value);
-        }
-        this->accountExists = true;
-        this->loggedIn = false;
-        acc.close();
-        this->debugOutput("Account data loaded from file!");
-    }
-
-    void saveConfigToFile(){
-        std::ofstream config("config.txt");
-        config << "debug=" << this->debug << "\n";
-        config.close();
-        this->debugOutput("Config saved to file!");
-    }
-
-    void loadConfigFromFile(){
-        std::ifstream config("config.txt");
-        std::string data;
-        int version;
-        while(std::getline(config, data)){
-            auto pos = data.find('=');
-            if(pos == std::string::npos) continue;
-
-            std::string key = data.substr(0, pos);
-            std::string value = data.substr(pos + 1);
-            if(key == "version") version = std::stoi(value);
-            else if(key == "debug") this->debug = (value == "1" ? true : false);
-        }
-        config.close();
-        this->debugOutput("Config loaded from file!");
-    }
-
-    bool checkConfigFile(){
-        std::ifstream config("config.txt");
-        if(!config.is_open() || std::filesystem::is_empty("config.txt")){
-            this-> debugOutput("Config file not found / no data!");
-            return false;
-        }
-        else {
-            this->debugOutput("Config file found!");
-            config.close();
-            return true;
-        }
-    }
-
-    void generateInterestRate(){
-        static std::random_device rd; //Why ts gotta be so complicated
-        static std::mt19937 gen(rd());
-        std::uniform_real_distribution<> dis(1.02, 1.04); //Pick random interest between 2% and 4%. ofc with 5 trillion decimals :/
-        this->interest = dis(gen);
-        this->debugOutput("Generated new interest rate: " + std::to_string(this->interest));
-    }
-
-    void calculateInterest(){
-        this->money = money * interest;
-        std::time_t time = this->getCurrentTime();
-        this->addHistory("Interest of " + std::to_string(this->interest) + " applied on " + std::ctime(&time));
-        this->debugOutput("Interest of " + std::to_string(this->interest) + " applied! New balance: " + std::to_string(this->money));
-    }
-
-
-    std::string getFullDob(){return std::to_string(this->getDobDay()) + "/" + std::to_string(this->getDobMonth()) + "/" + std::to_string(this->getDobYear());}
-};
+#include "account/account.h"
+#include "logger/logger.h"
 
 int main(){
     Account account("John Pork");
     
-    if(account.getYear() < account.getCurrentYear()){
-        account.calculateInterest(); //This.... could be bad? Unless it gives issues, ill ignore it ig
-    }
-
     if(account.checkConfigFile()){ //note to self: load config first, so that debugOutput works lol
         account.loadConfigFromFile();
     }
     if(account.checkAccountFile()){
         account.loadAccountFromFile();
     }
+    if(account.getYear() < account.getCurrentYear() && account.getAccountExists()){
+        account.calculateInterest(); //This.... could be bad? Unless it gives issues, ill ignore it ig
+    }
     account.setYear(account.getCurrentYear()); //is this... like smart?? I gots no clue
-
     
     std::string input;
     while (true){
@@ -304,7 +108,13 @@ int main(){
                     year += c;
                 }
             }
-            account = Account(name, password, {std::stoi(year), std::stoi(month), std::stoi(day)}, 0);
+            account.setName(name);
+            account.setPassword(password);
+            account.setDobDay(std::stoi(day));
+            account.setDobMonth(std::stoi(month));
+            account.setDobYear(std::stoi(year));
+            account.setAccountExists(true);
+            account.setLoggedIn(true);
             std::cout << "\nAccount created successfully!\nWelcome to Bloo's banking service, " << account.getName() << "!\n";
         }
         else if(input == "1" && account.getAccountExists() && !account.getLoggedIn()){
@@ -338,6 +148,7 @@ int main(){
                 << "| 6. Debug: currently " << (account.getDebug() ? "enabled\n" : "disabled\n")
                 << (account.getDebug() ? "| 7. Simulate Interest\n" : "")
                 << (account.getDebug() ? "| 8. Generate New Interest Rate\n" : "")
+                << (account.getDebug() ? "| 9. Delete Log\n" : "")
                 << "| To go back, type 'end'\n";
                 std::cout << ">> ";
                 std::getline(std::cin, input);
@@ -382,7 +193,8 @@ int main(){
                     break;
                 }
                 else if(input == "5"){
-                    account = Account("John Pork");
+                    account.setName("John Pork");
+                    account.setAccountExists(false);
                     std::ofstream acc("Account.txt", std::ofstream::trunc);
                     acc.close();
                     std::cout << "\nAccount deleted successfully!\n";
@@ -398,15 +210,17 @@ int main(){
                 else if(input == "8" && account.getDebug()){
                     account.generateInterestRate();
                 }
+                else if(input == "9" && account.getDebug()){
+                    std::ofstream logfile("log.txt", std::ofstream::trunc); //horrible, fix later
+                    logfile.close();
+                }
             }
         }
         else if(input == "end" || input == "END"){
-            account.saveAccountToFile();
+            if(account.getAccountExists()) account.saveAccountToFile();
             account.saveConfigToFile();
             std::cout << "\nThank you for banking with us!\nGood bye!\n";
             break;
         }
     }
-    
-    
 }
